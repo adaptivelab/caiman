f17ad30a6538078ce73fc66e2f975b8e5f97ccbc
Rob Berry <rob@luvhate.us>
2013-02-26 12:11:03 +0000

    Added jenkins build file

---
+++ jenkins.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ -d ".env" ]; then
+ echo "**> virtualenv exists"
+else
+ echo "**> creating virtualenv"
+ virtualenv .env
+ mkdir -p $HOME/pip-cache
+ .env/bin/pip install --download-cache $HOME/pip-cache nose==1.1.2 coverage pep8 pylint clonedigger
+ .env/bin/pip install -q fudge==1.0.3 pytest==2.3.4
+fi
+
+. .env/bin/activate
+.env/bin/python setup.py install
+py.test -q --cov caiman .
+coverage xml
+pep8 caiman | tee pep8.out
