bcc4d0091d0675a2731b54b3536e07bedf137a81
Rob Berry <rob@luvhate.us>
2013-02-26 12:14:21 +0000

    Add pytest-cov to build requirements

--- jenkins.sh
+++ jenkins.sh
@@ -6,9 +6,10 @@ else
  echo "**> creating virtualenv"
  virtualenv .env
  mkdir -p $HOME/pip-cache
- .env/bin/pip install --download-cache $HOME/pip-cache nose==1.1.2 coverage pep8 pylint clonedigger
+ .env/bin/pip install --download-cache $HOME/pip-cache pytest-cov coverage pep8 pylint clonedigger
  .env/bin/pip install -q fudge==1.0.3 pytest==2.3.4
 fi
+.env/bin/pip install pytest-cov
 
 . .env/bin/activate
 .env/bin/python setup.py install
